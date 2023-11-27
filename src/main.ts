035732884aeaca60adf9cf4c51116552b9777b88
Gnlow <gnlow@protonmail.com>
2023-11-27 13:15:54 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,31 @@ import {
     View,
     OSM,
     VectorSource,
+    TileLayer,
+    VectorLayer,
 } from "./deps/ol.ts"
+
+const source = new VectorSource({wrapX: false})
 
 new Map({
     target: "map",
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        }),
+        new VectorLayer({
+            source,
+        })
+    ],
+    view: new View({
+        center: [-11000000, 4600000],
+        zoom: 4,
+    }),
+    interactions: [
+        new Draw({
+            source,
+            type: "LineString",
+        })
+    ]
 })
+
