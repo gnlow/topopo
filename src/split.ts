5d146d06eb82149c74cec15468ba180988ad050b
Gnlow <gnlow@protonmail.com>
2023-11-28 14:22:30 +0900

    Update split.ts

--- src/split.ts
+++ src/split.ts
@@ -8,8 +8,6 @@ import {
     LineString,
 } from "./deps/ol.ts"
 
-import { union } from "./deps/turf.ts"
-
 import { assert } from "./util/assert.ts"
 
 export const split =
@@ -21,15 +19,12 @@ export const split =
     draw.on("drawstart", e => {
         const geometry = e.feature.getGeometry()
         assert(geometry)
-        geometry.on("change", e => {
-            console.log(e.target)
-        })
+        geometry.on("change", onGeomChange)
     })
     return draw
 }
 
 const onGeomChange = (e: BaseEvent) => {
-    const format = new GeoJSON()
-    const turfLine = format.writeFeatureObject(new Feature(e.target as LineString))
-    union
+    const lineString = e.target as LineString
+    console.log(lineString.getCoordinates())
 }
